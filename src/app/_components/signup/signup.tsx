d7c918d5534cd90e3504f4966aadc00d640d0ec1
K3ndev <racelisjk@gmail.com>
2024-02-17 09:47:39 +0800

    chore: remove comments

--- src/app/_components/signup/signup.tsx
+++ src/app/_components/signup/signup.tsx
@@ -33,19 +33,6 @@ export default function SignUp({children}: {children: ReactNode}) {
           </div>
 
           <div className="mx-auto flex  w-full min-w-[15rem] max-w-[22rem] flex-col">
-            {/* <Form action={signup} >
-              <div className="mb-10">
-                <h1 className="mb-9 text-2xl sm:text-3xl">Create your account</h1>
-                <div >
-                  <Input name="username" id="username" type="text" placeholder="Username" className="mb-5 text-black" />
-                  <Input name="password" id="password" type="password" placeholder="Password" className="text-black" />
-                </div>
-              </div>
-
-              <div>
-                <Button className="w-full">Continue </Button>
-              </div>
-            </Form> */}
             {children}
           </div>
         </div>
